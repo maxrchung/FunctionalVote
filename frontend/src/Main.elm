5476a30addbccf70b338feee3b595b81a58340c5
maxrchung <maxrchung@gmail.com>
2020-02-11 23:53:39 -0800

    Initial navigation setup following Elm tutorials

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -9,84 +9,107 @@ import Http
 import Browser.Navigation as Navigation
 import Json.Decode as Decode
 import Json.Encode as Encode
+import Url
+import Url.Parser as Parser
+import Url.Parser.Query as Query
 
 -- MAIN
 main = 
-    Browser.element 
-        { init = init
-        , update = update
-        , subscriptions = subscriptions
-        , view = view 
-        }
+  Browser.application 
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    , onUrlRequest = UrlRequested
+    , onUrlChange = UrlChanged
+    }
 
 -- MODEL
 type alias Model = 
-    { title : String
-    , choices : Array String }
+  { key: Navigation.Key
+  , title : String
+  , choices : Array String }
 
-init : () -> (Model, Cmd Msg)
-init _ = (Model "" (Array.fromList ["", "", ""]), Cmd.none)
+init : () -> Url.Url -> Navigation.Key -> ( Model, Cmd Msg )
+init flags url key
+  = (Model key "" (Array.fromList ["", "", ""]), Cmd.none)
 
 -- UPDATE
 type Msg 
-    = ChangeTitle String
-    | ChangeChoice Int String
-    | MakePollRequest
-    | MakePollResponse (Result Http.Error String)
+  = ChangeTitle String
+  | ChangeChoice Int String
+  | MakePollRequest
+  | MakePollResponse (Result Http.Error String)
+  | UrlRequested Browser.UrlRequest
+  | UrlChanged Url.Url
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     ChangeTitle newTitle ->
-        ({ model | title = newTitle }, Cmd.none)
-        
+      ({ model | title = newTitle }, Cmd.none)
+      
     ChangeChoice index newChoice ->
-        ({ model | choices = Array.set index newChoice model.choices }, Cmd.none)
+      ({ model | choices = Array.set index newChoice model.choices }, Cmd.none)
 
     MakePollRequest ->
-        (model, makePollRequest model)
+      (model, makePollRequest model)
 
     MakePollResponse result ->
-        case result of
-            Ok pollID ->
-                (model, Navigation.load ("/poll/" ++ pollID) )
-            Err _ ->
-                (model, Cmd.none)
-            
+      case result of
+        Ok pollId ->
+          (model, Navigation.load ("/poll/" ++ pollId) )
+        Err _ ->
+          (model, Cmd.none)
+
+    UrlRequested urlRequest ->
+      case urlRequest of
+        Browser.Internal url ->
+          ( model, Navigation.pushUrl model.key (Url.toString url) )
+
+        Browser.External href ->
+          ( model, Navigation.load href )
+
+    UrlChanged url ->
+      ( model, Cmd.none )
+      
 
 -- SUBSCRIPTIONS
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-    Sub.none
+  Sub.none
 
 -- VIEW
-view : Model -> Html Msg
+view : Model -> Browser.Document Msg
 view model =
-    div []
+  { title = "Functional Vote" 
+  , body = 
+    [ div []
         ([ input [ placeholder "Title", value model.title, onInput ChangeTitle ] [] ] ++
         Array.toList (Array.indexedMap renderChoice model.choices) ++
         [ button [onClick MakePollRequest] [ text "Create Poll" ] ])
-
+    ]
+  }
 
 renderChoice : Int -> String -> Html Msg
 renderChoice index choice =
-    input [ placeholder "Choice", value choice, onInput (ChangeChoice index) ] []
+  input [ placeholder "Choice", value choice, onInput (ChangeChoice index) ] []
 
 makePollRequest : Model -> Cmd Msg
 makePollRequest model =
-    Http.post
-        { url = "http://localhost:4000/poll/"
-        , body = Http.jsonBody (makePollJSON model)
-        , expect = Http.expectJson MakePollResponse makePollDecoder
-        }
+  Http.post
+    { url = "http://localhost:4000/poll/"
+    , body = Http.jsonBody (makePollJSON model)
+    , expect = Http.expectJson MakePollResponse makePollDecoder
+    }
 
 makePollJSON : Model -> Encode.Value
-makePollJSON model =
-    Encode.object
-        [ ( "title", Encode.string model.title )
-        , ( "choices", Encode.array Encode.string model.choices )
-        ]
+makePollJSON model = 
+  Encode.object
+    [ ( "title", Encode.string model.title )
+    , ( "choices", Encode.array Encode.string model.choices )
+    ]
 
 makePollDecoder : Decode.Decoder String
 makePollDecoder =
-    Decode.field "data" (Decode.field "image_url" Decode.string)
+  Decode.field "id" (Decode.field "id" Decode.string)
