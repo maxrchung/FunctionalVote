21b8f274249222908ef10261fc51196f96445513
maxrchung <maxrchung@gmail.com>
2020-02-15 22:07:39 -0800

    Added poll page

--- frontend/src/Page/Poll.elm
+++ frontend/src/Page/Poll.elm
@@ -1,36 +1,78 @@
 module Page.Poll exposing (..)
 
-import Browser
+import Browser.Navigation as Navigation
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-import Array exposing (..)
+import Dict
+import Http
+import Json.Decode as Decode
 
 
 
 -- MODEL
 type alias Model = 
-  { id: Int }
+  { id: Int,
+    poll: Poll
+  }
+
+type alias Poll =
+  { title: String,
+    choices: List String,
+    winner: String
+  }
 
 init : Int -> ( Model, Cmd Msg )
 init id = 
-  ( Model id, Cmd.none )
+  let model = Model id (Poll "" [] "")
+  in ( model, getPollRequest model )
 
 
 
 -- UPDATE
 type Msg 
-  = PollTest
+  = GetPollResponse (Result Http.Error Poll)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    PollTest ->
-      (model, Cmd.none)
+    GetPollResponse result ->
+      case result of
+        Ok newPoll ->
+          ( { model | poll = newPoll }, Cmd.none )
+
+        Err _ ->
+          ( model, Cmd.none )
+
+getPollRequest : Model -> Cmd Msg
+getPollRequest model =
+  Http.get
+    { url = "http://localhost:4000/poll/" ++ String.fromInt model.id
+    , expect = Http.expectJson GetPollResponse getPollDecoder
+    }
+
+getPollDecoder : Decode.Decoder Poll
+getPollDecoder =
+  Decode.map3 Poll
+    (Decode.field "data" (Decode.field "title" Decode.string))
+    (Decode.field "data" (Decode.field "choices" (Decode.list Decode.string)))
+    (Decode.field "data" (Decode.field "winner" Decode.string))
 
 
 
 -- VIEW
 view : Model -> Html Msg
 view model =
-  div [] [ text ("I am poll " ++ String.fromInt model.id) ]
+  div [] 
+    ( [ h1 [ placeholder "Title" ] [ text model.poll.title ] ] ++
+
+      List.map (renderChoice model.poll.winner) model.poll.choices
+    )
+
+renderChoice : String -> String -> Html Msg
+renderChoice winner choice =
+  if winner == choice then
+    h2 [] [ text <| "Winner: " ++ choice ]
+  else
+    div [] [ text <| "Not winner: " ++ choice]
+    
