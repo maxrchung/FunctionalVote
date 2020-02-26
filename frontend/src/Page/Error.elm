6e9ce51c157376845a1628458c2511620f821b1b
maxrchung <maxrchung@gmail.com>
2020-02-26 00:17:25 -0800

    Added text for Error page

--- frontend/src/Page/Error.elm
+++ frontend/src/Page/Error.elm
@@ -9,4 +9,12 @@ import Html.Events exposing (..)
 -- VIEW
 view : Html a
 view =
-  div [] [ text "Error" ]
+  div [] 
+    [ h1 [ class "fv-main-header" ]
+      [ text "Error" ]
+    ,  div [ class "fv-main-text pb-6" ]
+      [ text "-- Hmm, this page doesn’t seem to exist, or maybe we encountered an error. Feel free to "
+      , a [ href "https://twitter.com/FunctionalVote" ] [ text "contact us"]
+      , text " if you’re experiencing any issues." ]
+    , 
+    ]
