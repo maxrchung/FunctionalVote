e95d9db6c1d79492701e57f5b43da4378456647f
maxrchung <maxrchung@gmail.com>
2020-03-19 02:50:02 -0700

    Added elm-visualization bar chart example

--- frontend/src/Page/Poll.elm
+++ frontend/src/Page/Poll.elm
@@ -8,6 +8,16 @@ import Html.Events exposing ( .. )
 import Http
 import Json.Decode as Decode
 
+import Axis
+import DateFormat
+import Scale exposing ( BandConfig, BandScale, ContinuousScale, defaultBandConfig )
+import Time
+import TypedSvg as Svg
+import TypedSvg.Attributes as SvgAttributes
+import TypedSvg.Attributes.InPx as SvgInPx
+import TypedSvg.Core as SvgCore
+import TypedSvg.Types as SvgTypes
+
 
 
 -- MODEL
@@ -189,4 +199,83 @@ view model =
           [ text "Submit Vote" ]
         , div [ class "w-8 text-right" ] [ text "" ]
         ]
+    ]
+
+
+
+w : Float
+w =
+  900
+
+
+h : Float
+h =
+  450
+
+
+padding : Float
+padding =
+  30
+
+
+xScale : List ( Time.Posix, Float ) -> BandScale Time.Posix
+xScale model =
+  List.map Tuple.first model
+      |> Scale.band { defaultBandConfig | paddingInner = 0.1, paddingOuter = 0.2 } ( 0, w - 2 * padding )
+
+
+yScale : ContinuousScale Float
+yScale =
+  Scale.linear ( h - 2 * padding, 0 ) ( 0, 5 )
+
+
+dateFormat : Time.Posix -> String
+dateFormat =
+  DateFormat.format [ DateFormat.dayOfMonthFixed, DateFormat.text " ", DateFormat.monthNameAbbreviated ] Time.utc
+
+
+xAxis : List ( Time.Posix, Float ) -> SvgCore.Svg msg
+xAxis model =
+  Axis.bottom [] (Scale.toRenderable dateFormat (xScale model))
+
+
+yAxis : SvgCore.Svg msg
+yAxis =
+  Axis.left [ Axis.tickCount 5 ] yScale
+
+
+column : BandScale Time.Posix -> ( Time.Posix, Float ) -> SvgCore.Svg msg
+column scale ( date, value ) =
+  Svg.g [ SvgAttributes.class [ "column" ] ]
+      [ Svg.rect
+          [ SvgInPx.x <| Scale.convert scale date
+          , SvgInPx.y <| Scale.convert yScale value
+          , SvgInPx.width <| Scale.bandwidth scale
+          , SvgInPx.height <| h - Scale.convert yScale value - 2 * padding
+          ]
+          []
+      , Svg.text_
+          [ SvgInPx.x <| Scale.convert (Scale.toRenderable dateFormat scale) date
+          , SvgInPx.y <| Scale.convert yScale value - 5
+          , SvgAttributes.textAnchor SvgTypes.AnchorMiddle
+          ]
+          [ SvgCore.text <| String.fromFloat value ]
+      ]
+
+
+viewChart : List ( Time.Posix, Float ) -> SvgCore.Svg msg
+viewChart model =
+  Svg.svg [ SvgAttributes.viewBox 0 0 w h ]
+    [ Svg.style [] [ SvgCore.text """
+        .column rect { fill: rgba(118, 214, 78, 0.8); }
+        .column text { display: none; }
+        .column:hover rect { fill: rgb(118, 214, 78); }
+        .column:hover text { display: inline; }
+      """ ]
+    , Svg.g [ SvgAttributes.transform [ SvgTypes.Translate (padding - 1) (h - padding) ] ]
+        [ xAxis model ]
+    , Svg.g [ SvgAttributes.transform [ SvgTypes.Translate (padding - 1) padding ] ]
+        [ yAxis ]
+    , Svg.g [ SvgAttributes.transform [ SvgTypes.Translate padding padding ], SvgAttributes.class [ "series" ] ] <|
+        List.map (column (xScale model)) model
     ]
