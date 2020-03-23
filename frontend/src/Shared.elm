c683e69722669fa9dad49dc7f94f6a382eb11fd9
maxrchung <maxrchung@gmail.com>
2020-03-22 19:51:50 -0700

    Share link icons

--- frontend/src/Shared.elm
+++ frontend/src/Shared.elm
@@ -1,6 +1,7 @@
 
 module Shared exposing ( .. )
 
+import FeatherIcons
 import Html exposing ( .. )
 import Html.Attributes exposing ( .. )
 
@@ -15,8 +16,65 @@ renderShareLinks url description =
         [ class "fv-main-text mb-2" ]
         [ text description ]
 
-    , div
-        []
-        [ 
+    , div 
+        [ class "flex justify-between" ]
+        [ div [ class "w-8" ] [ text "" ]
+
+        , div
+            [ class "w-full" ]
+            [ div 
+                [ class "flex justify-between w-full" ]
+                [ div 
+                    [ class "w-8 text-blue-500" ] 
+                    [ renderIcon FeatherIcons.share ]
+
+                , div
+                    [ class "flex-grow bg-orange-900" ]
+                    [ text "asdf" ]
+
+                , div 
+                    [ class "w-8 text-orange-500" ] 
+                    [ renderIcon FeatherIcons.clipboard ]
+                ]
+            
+            , div 
+                [ class "flex justify-between w-full" ]
+                [ div 
+                    [ class "w-8 text-blue-500" ] 
+                    [ renderIcon FeatherIcons.facebook ]
+
+                , div
+                    [ class "flex-grow bg-orange-900" ]
+                    [ text "asdf" ]
+
+                , div 
+                    [ class "w-8 text-orange-500" ] 
+                    [ renderIcon FeatherIcons.clipboard ]
+                ]
+
+            , div 
+                [ class "flex justify-between w-full" ]
+                [ div 
+                    [ class "w-8 text-blue-500" ] 
+                    [ renderIcon FeatherIcons.twitter ]
+
+                , div
+                    [ class "flex-grow bg-orange-900" ]
+                    [ text "asdf" ]
+
+                , div 
+                    [ class "w-8 text-orange-500" ] 
+                    [ renderIcon FeatherIcons.clipboard ]
+                ]
+            ]
+          
+        , div [ class "w-8 text-right" ] [ text "" ]
         ]
     ]
+
+
+renderIcon : FeatherIcons.Icon -> Html a
+renderIcon icon =
+  icon
+    |> FeatherIcons.withSize 22
+    |> FeatherIcons.toHtml []
