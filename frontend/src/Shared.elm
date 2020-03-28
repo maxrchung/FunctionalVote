b06954f0c442c1402d154ef6d6aee548e6f8bd7d
maxrchung <maxrchung@gmail.com>
2020-03-27 19:11:40 -0700

    Disable share link inputs

--- frontend/src/Shared.elm
+++ frontend/src/Shared.elm
@@ -33,6 +33,7 @@ renderShareLinks url helpText title message =
                     , id "fv-share-link" ]
                     [ input
                         [ class "fv-share-link fv-input"
+                        , disabled True
                         , value <| url
                         ]
                         []
@@ -62,6 +63,7 @@ renderShareLinks url helpText title message =
                     [ class "mx-2 w-full" ]
                     [ input
                         [ class "fv-input"
+                        , disabled True
                         , value twitterUrl
                         ]
                         []
@@ -83,6 +85,7 @@ renderShareLinks url helpText title message =
                     [ class "mx-2 w-full" ]
                     [ input
                         [ class "fv-input"
+                        , disabled True
                         , value <| facebookUrl
                         ]
                         []
@@ -104,6 +107,7 @@ renderShareLinks url helpText title message =
                     [ class "mx-2 w-full" ]
                     [ input
                         [ class "fv-input"
+                        , disabled True
                         , value <| mailUrl
                         ]
                         []
