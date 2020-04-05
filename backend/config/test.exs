6a2f3153745ee8733676cb150dcf8c1c727e43e8
Xenocidel <aliao4@uci.edu>
2020-04-04 23:23:34 -0700

    add untracked vote submission tests

--- backend/config/test.exs
+++ backend/config/test.exs
@@ -4,7 +4,7 @@ use Mix.Config
 config :functional_vote, FunctionalVote.Repo,
   username: "postgres",
   password: "postgres",
-  database: "functional_vote_dev",
+  database: "functional_vote_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
