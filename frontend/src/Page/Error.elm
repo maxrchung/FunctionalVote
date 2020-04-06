caa3e4a8ac7eebab0b517bc2b8435e2ddb09a079
maxrchung <maxrchung@gmail.com>
2020-04-06 16:01:49 -0700

    Remove Error page update and message

--- frontend/src/Page/Error.elm
+++ frontend/src/Page/Error.elm
@@ -1,32 +1,13 @@
 module Page.Error exposing ( .. )
 
-import Browser.Navigation as Navigation
 import Html exposing ( .. )
 import Html.Attributes exposing ( .. )
 import Html.Events exposing ( .. )
 
 
 
-type alias Model = { key : Navigation.Key }
-
-init : Navigation.Key -> ( Model, Cmd Msg )
-init key = ( Model key, Cmd.none )
-
-
-
--- UPDATE
-type Msg 
-  = GoToHome
-
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg model =
-  case msg of
-    GoToHome ->
-      ( model, Navigation.pushUrl model.key "/" )
-
--- VIEW
-view : Model -> Html Msg
-view _ =
+view : Html a
+view =
   div [] 
     [ div [ class "fv-code" ] [ text "{-" ]
       
@@ -38,15 +19,15 @@ view _ =
       , a [ href "https://twitter.com/FunctionalVote" 
           , target "_blank"
           ] [ text "contact us"]
-      , text " if you’re experiencing any issues." ]
+      , text " if you are experiencing any issues." ]
 
     , div [ class "fv-code mb-1" ] [ text "-}" ]
 
     , div [ class "flex justify-between" ]
         [ div [ class "w-8" ] [ text "" ]
-        , button 
+        , a 
             [ class "fv-btn"
-            , onClick GoToHome
+            , href "/"
             ]
             [ text "Go Home" ] 
         , div [ class "w-8 text-right" ] [ text "" ]
