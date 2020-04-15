633008f51270775283eeb00fcf27b418dde2a7b4
maxrchung <maxrchung@gmail.com>
2020-04-15 13:00:04 -0700

    Add recaptcha Elixir package

--- backend/mix.exs
+++ backend/mix.exs
@@ -20,7 +20,7 @@ defmodule FunctionalVote.MixProject do
   def application do
     [
       mod: {FunctionalVote.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :recaptcha]
     ]
   end
 
@@ -43,7 +43,8 @@ defmodule FunctionalVote.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:cors_plug, "~> 2.0"}
+      {:cors_plug, "~> 2.0"},
+      {:recaptcha, "~> 3.0"}
     ]
   end
 
