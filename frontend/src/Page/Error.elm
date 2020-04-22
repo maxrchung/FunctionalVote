70a92e4eab50c1be9e4bc929d186497b3ed556b7
maxrchung <maxrchung@gmail.com>
2020-04-22 15:33:27 -0700

    Fix Error page styling

--- frontend/src/Page/Error.elm
+++ frontend/src/Page/Error.elm
@@ -9,10 +9,11 @@ import Html.Events exposing ( .. )
 view : Html a
 view =
   div []
-    [ div [ class "fv-code" ] [ text "{-" ]
-
-    , h1 [ class "fv-header" ]
-      [ text "Error" ]
+    [ div [ class "flex justify-between" ]
+        [ div [ class "fv-code w-8" ] [ text "{-" ]
+        , h1 [ class "fv-header" ] [ text "Error" ]
+        , div [ class "w-8 text-right" ] [ text "" ]
+        ]
 
     ,  div [ class "fv-text mb-2" ]
       [ text "Hmm, this page doesn’t seem to exist, or maybe we encountered an error. Feel free to "
