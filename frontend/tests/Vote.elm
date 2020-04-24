04d8708f0b9269b05c0a5ebc78eac39b9e480f83
maxrchung <maxrchung@gmail.com>
2020-04-24 00:22:58 -0700

    Wrote test cases for rankValue

--- frontend/tests/Vote.elm
+++ frontend/tests/Vote.elm
@@ -1,18 +1,18 @@
-module VoteOrdering exposing ( .. )
+module Vote exposing ( .. )
 
 import Test exposing ( .. )
 import Expect
 import Dict
-import Page.Vote exposing ( changeRank )
+import Page.Vote as Vote
 
 
 
-voteOrdering : Test
-voteOrdering =
-  describe "Vote Ordering"
+changeRank : Test
+changeRank =
+  describe "Change Rank"
     [ test "add 1 choice to ordered" <|
         \_ ->
-          changeRank
+          Vote.changeRank
             "1"
             "c1"
             Dict.empty
@@ -26,7 +26,7 @@ voteOrdering =
 
     , test "add all to ordered" <|
         \_ ->
-          changeRank
+          Vote.changeRank
             "2"
             "c2"
             ( Dict.fromList
@@ -44,7 +44,7 @@ voteOrdering =
 
     , test "ordered remove goes to unordered bottom" <|
       \_ ->
-        changeRank
+        Vote.changeRank
           "--"
           "c1"
           ( Dict.fromList
@@ -62,7 +62,7 @@ voteOrdering =
 
     , test "bumps rank 1 choice to rank 2" <|
       \_ ->
-        changeRank
+        Vote.changeRank
           "1"
           "c2"
           ( Dict.fromList
@@ -80,7 +80,7 @@ voteOrdering =
 
     , test "bumps last choice to unordered" <|
       \_ ->
-        changeRank
+        Vote.changeRank
           "2"
           "c1"
           ( Dict.fromList
@@ -98,7 +98,7 @@ voteOrdering =
 
     , test "bumps unfilled space and stops" <|
       \_ ->
-        changeRank
+        Vote.changeRank
           "1"
           "c1"
           ( Dict.fromList
@@ -119,7 +119,7 @@ voteOrdering =
 
     , test "bumps multiple unfilled spaces and stops" <|
       \_ ->
-        changeRank
+        Vote.changeRank
           "1"
           "c1"
           ( Dict.fromList
@@ -137,4 +137,44 @@ voteOrdering =
                   ]
               , [ "c5", "c6" ]
               )
+    ]
+
+rankValue : Test
+rankValue =
+  describe "Rank Value"
+    [ test "1 -> 1st" <|
+        \_ -> Vote.rankValue 1 |> Expect.equal "1st"
+
+    , test "2 -> 2nd" <|
+        \_ -> Vote.rankValue 2 |> Expect.equal "2nd"
+
+    , test "3 -> 3rd" <|
+        \_ -> Vote.rankValue 3 |> Expect.equal "3rd"
+
+    , test "4 -> 4th" <|
+        \_ -> Vote.rankValue 4 |> Expect.equal "4th"
+
+    , test "11 -> 11th" <|
+        \_ -> Vote.rankValue 11 |> Expect.equal "11th"
+
+    , test "12 -> 12th" <|
+        \_ -> Vote.rankValue 12 |> Expect.equal "12th"
+
+    , test "13 -> 13th" <|
+        \_ -> Vote.rankValue 13 |> Expect.equal "13th"
+
+    , test "14 -> 14th" <|
+        \_ -> Vote.rankValue 14 |> Expect.equal "14th"
+
+    , test "21 -> 21st" <|
+        \_ -> Vote.rankValue 21 |> Expect.equal "21st"
+
+    , test "100 -> 100th" <|
+        \_ -> Vote.rankValue 100 |> Expect.equal "100th"
+
+    , test "102 -> 102nd" <|
+        \_ -> Vote.rankValue 102 |> Expect.equal "102nd"
+
+    , test "112 -> 112th" <|
+        \_ -> Vote.rankValue 112 |> Expect.equal "112th"
     ]
