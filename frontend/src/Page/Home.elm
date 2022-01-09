dc28b276b37ec4fc6a870622df6a40b7f8618ec2
maxrchung <maxrchung@gmail.com>
2022-01-08 19:09:50 -0800

    update wording

--- frontend/src/Page/Home.elm
+++ frontend/src/Page/Home.elm
@@ -285,7 +285,7 @@ view model =
 
               , p [ class "fv-text mb-6" ] [ text "Ranked-choice voting is typically fairer than traditional voting because preferential ranking is more flexible than casting a single vote in stone. Voters are incentivized to vote for their most preferred options rather than select a popular choice." ]
 
-              , p [ class "fv-text mb-6" ] [ text "There are many resources online that go over ranked-choice voting. We particularly like a video by "
+              , p [ class "fv-text mb-6" ] [ text "There are many resources online that go over ranked-choice voting in greater detail. We particularly like a video by "
                   , a
                       [ href "https://www.youtube.com/user/CGPGrey"
                       , target "_blank"
