bdb6742e15808bc8e25f69c5935fa69ad3db325d
maxrchung <maxrchung@gmail.com>
2022-01-09 00:53:39 -0800

    remove assets

--- backend/config/dev.exs
+++ backend/config/dev.exs
@@ -20,15 +20,7 @@ config :functional_vote, FunctionalVoteWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [
-    node: [
-      "node_modules/webpack/bin/webpack.js",
-      "--mode",
-      "development",
-      "--watch-stdin",
-      cd: Path.expand("../assets", __DIR__)
-    ]
-  ]
+  watchers: []
 
 # ## SSL Support
 #
