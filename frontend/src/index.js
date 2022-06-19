ce3ba7f0d623436e202b44b8a9e86062800d1d7d
maxrchung <maxrchung@gmail.com>
2022-06-19 15:56:03 -0700

    add recaptcha public key to frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,32 +1,34 @@
-import './main.css';
-import { Elm } from './Main.elm';
-import * as serviceWorker from './serviceWorker';
+import './main.css'
+import { Elm } from './Main.elm'
+import * as serviceWorker from './serviceWorker'
 
 const app = Elm.Main.init({
   node: document.getElementById('root'),
   flags: process.env.NODE_ENV
-});
+})
 app.ports.renderRecaptcha.subscribe(() => {
   requestAnimationFrame(() => {
     grecaptcha.render('recaptcha', {
-      callback: val => {
-        app.ports.submitRecaptcha.send(val);
+      // Seems like in production we need the recaptcha
+      ...(process.env.NODE_ENV === 'production' && { sitekey: process.env.RECAPTCHA_PUBLIC_KEY }),
+      callback: (val) => {
+        app.ports.submitRecaptcha.send(val)
       }
-    });
-  });
-});
+    })
+  })
+})
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
 
 const showShareText = () => {
-  const texts = document.getElementsByClassName('fv-share-text');
+  const texts = document.getElementsByClassName('fv-share-text')
   for (let text of texts) {
-    text.classList.remove('hidden');
+    text.classList.remove('hidden')
   }
-};
+}
 
-const clipboard = new ClipboardJS('.fv-share-copy');
-clipboard.on('success', showShareText);
+const clipboard = new ClipboardJS('.fv-share-copy')
+clipboard.on('success', showShareText)
