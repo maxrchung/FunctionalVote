8e312930b0b970b0d28d5fa93d9b451458a0748b
maxrchung <maxrchung@gmail.com>
2022-06-19 15:54:32 -0700

    update backend packages for local testing

--- backend/mix.exs
+++ backend/mix.exs
@@ -33,8 +33,8 @@ defmodule FunctionalVote.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.12"},
-      {:phoenix_pubsub, "~> 1.1"},
+      {:phoenix, "~> 1.5.9"},
+      {:phoenix_pubsub, "~> 2.0"},
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.1"},
       {:postgrex, ">= 0.0.0"},
